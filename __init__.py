69574c813e78af1d017eb7e21139cce62f96703c
Keith Bechtol <bechtol@des10.fnal.gov>
2013-06-30 19:04:45 -0500

    Initial import of main init file.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+This is the Ultra-faint Galaxy Likelihood (UGaLi) software package.
+
+Sub-packages contained are:
+  observation: 
+
+Other directories:
+  config_files: 
+"""
+
+import numpy
+
+import constants
