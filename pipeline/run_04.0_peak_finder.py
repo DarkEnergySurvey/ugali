a435ac90ca2d9463a7ec4202937f7e38c6a83669
Alex Drlica-Wagner <kadrlica@fnal.gov>
2014-03-30 20:46:18 -0700

    Implementation of pipeline tasks

--- pipeline/run_04.0_peak_finder.py
+++ pipeline/run_04.0_peak_finder.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python
 
-####################################################
-### !!! PLACEHOLDER FOR FUTURE PIPELINE TASK !!! ###
-####################################################
+from ugali.candidate.search import CandidateSearch
+import ugali.candidate.associate
 
+from ugali.utils.parse_config import Config
+from ugali.utils.logger import logger
+
+COMPONENTS = ['label','objects','associate']
 if __name__ == "__main__":
     from optparse import OptionParser
     usage = "Usage: %prog  [options] config.py"
     description = "..."
     parser = OptionParser(usage=usage,description=description)
+    parser.add_option('-v','--verbose', action='store_true')
+    parser.add_option('-r','--run', default=[],
+                      action='append',choices=COMPONENTS,
+                      help="Choose analysis component to run")
     (opts, args) = parser.parse_args()
-    raise Exception("NOT IMPLEMENTED")
+    if not opts.run: opts.run = COMPONENTS
+    if opts.verbose: logger.setLevel(logger.DEBUG)
+
+    configfile = args[0]
+    config = Config(configfile)
+
+    search = CandidateSearch(config)
+
+    if 'label' in opts.run:
+        logger.info("Running 'label'...")
+        search.createLabels()
+        search.writeLabels()
+    if 'objects' in opts.run:
+        logger.info("Running 'objects'...")
+        search.loadLabels()
+        search.createObjects()
+        search.writeObjects()
+    if 'associate' in opts.run:
+        logger.info("Running 'associate'...")
+        x,o = ugali.candidate.associate.associate_sources(config)
+        
+        
