75e400962139b26c40a09a5a35d8da47e0a046a9
KeithBechtol <keithbechtol@eagleheights-105-137.resnet.wisc.edu>
2015-06-22 23:13:44 -0500

    Better conforming with UGALI standards

--- preprocess/dotter.py
+++ preprocess/dotter.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
 """
 Script to automate the process of generating an isochrone library using the Dotter isochrones.
+
+Download isochrones from
+http://stellar.dartmouth.edu/models/isolf_new.html
 """
 
 import urllib2
@@ -7,9 +11,12 @@ import os
 import sys
 import numpy
 
+import ugali.utils.logger
+import ugali.utils.shell
+
 ############################################################
 
-def getDotterIsochrone(age, z, afe='+0.4', clr='DECam'):
+def getDotterIsochrone(age, z, outdir, afe='+0.4', hel='Y=0.245+1.5*Z', clr='DECam', force=False):
     """
     See Vargas et al. 2013 for the distribution of alpha elements in dSphs
     http://adsabs.harvard.edu/abs/2013ApJ...767..134V
@@ -25,11 +32,21 @@ def getDotterIsochrone(age, z, afe='+0.4', clr='DECam'):
     best compromise.
     """
 
+    outfile = '%s/iso_a%.1f_z%.5f.dat'%(outdir, age, z)
+    if os.path.exists(outfile) and not force:
+        ugali.utils.logger.logger.warning('Found %s; skipping...'%(outfile))
+        return
+
+    ugali.utils.logger.logger.info('Downloading isochrone: %s (age=%.2fGyr, metallicity=%g)'%(os.path.basename(outfile), age, z))
+
+    ##### Hard-coded options from the Dotter web interface -- do not use blindly #####
+
     dict_clr = {'SDSS ugriz': '11',
                 'PanSTARRS': '12',
                 'DECam': '14'}
     if clr not in dict_clr.keys():
-        sys.exit('ERROR: Photometric system %s not in available options: %s'%(clr, ', '.join(dict_clr.keys())))
+        ugali.utils.logger.logger.error('Photometric system %s not in available options: %s'%(clr, ', '.join(dict_clr.keys())))
+        sys.exit()
 
     dict_afe = {'-0.2': '0',
                 '0 (scaled-solar)': '1',
@@ -38,34 +55,66 @@ def getDotterIsochrone(age, z, afe='+0.4', clr='DECam'):
                 '+0.6': '4',
                 '+0.8': '5'}
     if afe not in dict_afe.keys():
-        sys.exit('ERROR: [alpha/Fe] ratio %s not in available options: %s'%(afe, ', '.join(dict_afe.keys())))
+        ugali.utils.logger.logger.error('[alpha/Fe] ratio %s not in available options: %s'%(afe, ', '.join(dict_afe.keys())))
+        sys.exit()
+
+    dict_hel = {'Y=0.245+1.5*Z': '1',
+                'Y=0.33': '2',
+                'Y=0.40': '3'}
+    if hel not in dict_hel.keys():
+        ugali.utils.logger.logger.error('Helium abundance %s not in available options: %s'%(hel, ', '.join(dict_hel.keys())))
+        sys.exit()
+
+    #####
 
     z_solar = 0.02 # Dotter convention
     feh = numpy.log10(z / z_solar)
-    query = 'http://stellar.dartmouth.edu/models/isolf_new.php?int=1&out=1&age=%.1f&feh=%.5f&hel=1&afe=%i&clr=%i&flt=&bin=&imf=1&pls=&lnm=&lns='%(age, feh, dict_afe[afe], dict_clr[clr])
+    query = 'http://stellar.dartmouth.edu/models/isolf_new.php?int=1&out=1&age=%.1f&feh=%.5f&hel=%i&afe=%i&clr=%i&flt=&bin=&imf=1&pls=&lnm=&lns='%(age, 
+                                                                                                                                                   feh, 
+                                                                                                                                                   int(dict_hel[hel]), 
+                                                                                                                                                   int(dict_afe[afe]), 
+                                                                                                                                                   int(dict_clr[clr]))
     response = urllib2.urlopen(query)
     page_source = response.read()
     isochrone_id = page_source.split('tmp/tmp')[-1].split('.iso')[0]
     infile = 'http://stellar.dartmouth.edu/models/tmp/tmp%s.iso'%(isochrone_id)
-    command = 'wget -q %s'%(infile)
+    command = 'wget -q %s -O %s'%(infile, outfile)
     os.system(command)
-    outfile = 'iso_a%.1f_z%.5f.dat'%(age, z)
-    command = 'mv tmp%s.iso %s'%(isochrone_id, outfile)
-    print command
-    os.system(command)
+    #outfile = 'iso_a%.1f_z%.5f.dat'%(age, z)
+    #command = 'mv tmp%s.iso %s'%(isochrone_id, outfile)
+    #print command
+    #os.system(command)
 
 ############################################################
 
 if __name__ == "__main__":
+    #import ugali.utils.parser
+    #description = "Download Dotter isochrones"
+    #parser = ugali.utils.parser.Parser(description=description)
 
-    # Test
-    getDotterIsochrone(12.2, 1.1e-4)
+    #from ugali.utils.config import Config
+    #config = Config(opts.config)
+
+    outdir = '/Users/keithbechtol/Documents/DES/projects/mw_substructure/des/isochrones/dotter_v3/'
+
+    if outdir is None: outdir = './'
+    ugali.utils.shell.mkdir(outdir)
 
     z_array = 10**numpy.linspace(numpy.log10(1.e-4), numpy.log10(1.e-2), 33)
     age_array = numpy.arange(1., 13.5 + 0.1, 0.1)
 
+    # Test
+    getDotterIsochrone(12.2, 1.1e-4, outdir=outdir)
+    
+    print '\nIf this were not a test, this script would download the following isochrone grid...'
+
+    print '\nAges:'
+    print age_array
+    print '\nMetallicities:'
+    print z_array
+
     #for z in z_array:
     #    for age in age_array:
-    #        getDotterIsochrone(age, z)
+    #        getDotterIsochrone(age, z, outdir=outdir)
 
 ############################################################
