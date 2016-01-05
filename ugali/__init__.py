c273270c19799e9bf43c62cfbee7b63b76238798
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-01-05 07:26:21 -0800

    Added factories to __init__
    Former-commit-id: 01739ee7cd5983e179f32f4026a1f1f1e04fc9f4

--- ugali/__init__.py
+++ ugali/__init__.py
@@ -3,6 +3,8 @@ This is the Ultra-faint Galaxy Likelihood (UGaLi) software package.
 
 """
 from .version import version
+from analysis.isochrone import isochroneFactory
+from analysis.kernel import kernelFactory
 
 __author__ = "Keith Bechtol & Alex Drlica-Wagner"
 __email__ = "bechtol@kicp.uchicago.edu, kadrlica@fnal.gov"
