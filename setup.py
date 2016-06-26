a832f04cb5cdb3935e61c8c512acb9e07886d080
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-06-25 19:52:18 -0500

    Modifying setup install to deal with isochrones

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ import io
 
 try: 
     from setuptools import setup, find_packages
+    from setuptools.command.install import install as _install
 except ImportError: 
     from distutils.core import setup
+    from distutils.command.install import install as _install
     def find_packages():
         return ['ugali','ugali.analysis','ugali.config','ugali.observation',
                 'ugali.preprocess','ugali.simulation','ugali.candidate',
@@ -28,45 +30,76 @@ Natural Language :: English
 Topic :: Scientific/Engineering
 """
 ISOCHRONES = URL+'/releases/download/v1.5.2/ugali-isochrones-v1.5.2.tar.gz'
+UGALIDIR = "$HOME/.ugali"
 
 def read(filename):
     return open(os.path.join(HERE,filename)).read()
 
-def progress_bar(count, block_size, total_size):
-    block = 100*block_size/float(total_size)
-    progress = count*block
-    if progress % 1 < 1.01*block:
-        msg = '[{:51}] ({:d}%)\r'.format(int(progress//2)*'='+'>',int(progress))
-        #msg = '({:d}%)\r'.format(int(progress))
-        sys.stdout.write(msg)
-        sys.stdout.flush()
-    #percent = int(100*count*block_size/total_size)
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
     
+    Parameters:
+    -----------
+    question : a string that is presented to the user.
+    default  : the presumed answer if the user just hits <Enter>.
+               It must be "yes" (the default), "no" or None (meaning
+               an answer is required of the user).
 
-class ProgressFileObject(io.FileIO):
+    Return:
+    -------
+    answer   : boolean True for "yes" or False for "no"
+
+    http://stackoverflow.com/a/3041990/4075339
+    """
+    valid = {"yes": True, "y": True, "ye": True, 
+             "no": False, "n": False}
+             
+    if default is None:    prompt = " [y/n] "
+    elif default == "yes": prompt = " [Y/n] "
+    elif default == "no":  prompt = " [y/N] "
+    else: raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+class ProgressFileIO(io.FileIO):
     def __init__(self, path, *args, **kwargs):
         self._total_size = os.path.getsize(path)
         io.FileIO.__init__(self, path, *args, **kwargs)
 
     def read(self, size):
         count = self.tell()/size
-        progress_bar(count,size,self._total_size)
+        self.progress_bar(count,size,self._total_size)
         return io.FileIO.read(self, size)
+
+    @staticmethod
+    def progress_bar(count, block_size, total_size):
+        block = 100*block_size/float(total_size)
+        progress = count*block
+        if progress % 1 < 1.01*block:
+            msg = '[{:51}] ({:d}%)\r'.format(int(progress//2)*'='+'>',int(progress))
+            sys.stdout.write(msg)
+            sys.stdout.flush()
 
 class IsochroneCommand(distutils.cmd.Command):
     """ Command for downloading isochrone files """
     description = "install isochrone files"
     user_options = [
-        ('isochrone-path=',None,
-         'path to install isochrones (default: $HOME/.ugali)'),
-        ('no-isochrone',None,
-         'do not download and install isochrones (default: False)')
+        ('isochrones-path=',None,
+         'path to install isochrones (default: %s)'%UGALIDIR),
         ]
-    boolean_options = ['no-isochrone']
+    # Should the default path be set by install 'prefix'?
 
     def initialize_options(self):
-        self.isochrone_path = os.path.expandvars('$HOME/.ugali')
-        self.no_isochrone = False
+        self.isochrones_path = os.path.expandvars(UGALIDIR)
 
     def finalize_options(self):
         pass
@@ -76,21 +109,21 @@ class IsochroneCommand(distutils.cmd.Command):
         import tarfile
 
         print("installing isochrones")
-        print("creating %s"%self.isochrone_path)
-        if not os.path.exists(self.isochrone_path):
-            os.makedirs(self.isochrone_path)
+        if not os.path.exists(self.isochrones_path):
+            print("creating %s"%self.isochrones_path)
+            os.makedirs(self.isochrones_path)
 
-        os.chdir(self.isochrone_path)
+        os.chdir(self.isochrones_path)
 
         url = ISOCHRONES
         tarball = os.path.basename(url)
 
         print("downloading %s"%url)
-        urllib.urlretrieve(url, tarball, reporthook=progress_bar)
+        urllib.urlretrieve(url, tarball, reporthook=ProgressFileIO.progress_bar)
         print('')
 
         print("extracting %s"%tarball)
-        with tarfile.open(fileobj=ProgressFileObject(tarball),mode='r:gz') as tar:
+        with tarfile.open(fileobj=ProgressFileIO(tarball),mode='r:gz') as tar:
             tar.extractall()
             tar.close()
             print('')
@@ -99,18 +132,74 @@ class IsochroneCommand(distutils.cmd.Command):
         os.remove(tarball)
 
     def run(self):
-        if self.no_isochrone:
+        if self.dry_run:
             print("skipping isochrone download")
             return
 
-        if os.path.exists(os.path.join(self.isochrone_path,'isochrones')):
-            print("isochrone directory found; skipping")
+        if os.path.exists(os.path.join(self.isochrones_path,'isochrones')) \
+           and not self.force:
+            print("isochrone directory found; skipping installation")
             return
 
         self.install_isochrones()
 
+class install(_install):
+    """ 
+    Subclass the setuptools 'install' class to add isochrone options.
+    """
+
+    user_options = _install.user_options + [
+        ('isochrones',None,"download and install isochrone files"),
+        ('no-isochrones',None,"don't install isochrone files"),
+        ('isochrones-path=',None,"isochrone installation path")
+    ]
+    boolean_options = _install.boolean_options + ['isochrones','no-isochrones']
+
+    def initialize_options(self):
+        _install.initialize_options(self)
+        self.isochrones = None
+        self.no_isochrones = None
+        self.isochrones_path = None
+
+    def finalize_options(self):
+        _install.finalize_options(self)
+        if self.no_isochrones is False: self.isochrones = False
+        if self.isochrones is False: self.no_isochrones = False
+
+    def run(self):
+        # run superclass install
+        _install.run(self)
+
+        # Ask the user about isochrone installation
+        # ADW: consider moving to 'finalize_options'
+        if self.isochrones is None:
+            question = "Install isochrone files (~100MB)?"
+            self.isochrones = query_yes_no(question,default='no')
+
+            if self.isochrones and not self.isochrones_path:
+                question = "Isochrone install path (default: %s): "%UGALIDIR
+                sys.stdout.write(question)
+                path = raw_input()
+                self.isochrone_path = path if path else None
+
+        if self.isochrones: 
+            self.install_isochrones()
+        else:
+            print("skipping isochrone install")
+            
+    def install_isochrones(self):
+        """
+        Call to isochrone install command.
+        http://stackoverflow.com/a/24353921/4075339
+        """
+        if self.isochrones_path:
+            cmd_obj = self.distribution.get_command_obj('isochrones')
+            cmd_obj.isochrones_path = self.isochrones_path
+        self.run_command('isochrones')
+            
 CMDCLASS = versioneer.get_cmdclass()
 CMDCLASS['isochrones'] = IsochroneCommand
+CMDCLASS['install'] = install
 
 setup(
     name=NAME,
@@ -136,3 +225,5 @@ setup(
     platforms='any',
     classifiers = [_f for _f in CLASSIFIERS.split('\n') if _f]
 )
+
+
