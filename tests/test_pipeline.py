9eb01edabad3d6ff0d830b32439fea11f846baaa
Alex Drlica-Wagner <kadrlica@fnal.gov>
2018-05-03 23:46:52 -0700

    adding pipeline tests

---
+++ tests/test_pipeline.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+Testing the pipeline.
+"""
+__author__ = "Alex Drlica-Wagner"
+import glob
+import os
+import subprocess
+
+def _test_pipeline(n=2,args=None):
+    pipedir='./ugali/pipeline'
+    
+    script = glob.glob(os.path.join(pipedir,'run_%02d.0*.py'%n))[0]
+    cmd = script + ' ./ugali/config/config_test.yaml'
+    if args: cmd += ' '+args
+    subprocess.check_call(cmd,shell=True)
+
+def test_pipeline_02():
+    _test_pipeline(2)
+
+def test_pipeline_03():
+    _test_pipeline(3,'-q local --cel 53.92 -54.05')
+
+#def test_pipeline_04():
+#    test_pipeline(4)
+# 
+#def test_pipeline_05():
+#    test_pipeline(5)
+
+if __name__ == "__main__":
+    import argparse
+    description = __doc__
+    parser = argparse.ArgumentParser(description=description)
+    args = parser.parse_args()
+
+    test_pipeline_03()
