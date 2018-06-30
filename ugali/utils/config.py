5d4530718e6e760c0c1423be979ff7dc043e7ee8
Alex Drlica-Wagner <kadrlica@fnal.gov>
2018-06-30 14:52:09 -0700

    faster file loading for large nside

--- ugali/utils/config.py
+++ ugali/utils/config.py
@@ -6,6 +6,7 @@ from os.path import join, exists
 import pprint
 import copy
 from collections import OrderedDict as odict
+import glob
 
 import numpy as np
 import healpy as hp
@@ -27,7 +28,7 @@ class Config(dict):
         Provides functionality to merge with a default configuration.
 
         Parameters:
-          input:   Either filename or dictionary (deep copied)
+          input:   Filename, dict, or Config (deep copied)
           default: Default configuration to merge
         
         Returns:
@@ -36,20 +37,25 @@ class Config(dict):
         self.update(self._load(default))
         self.update(self._load(input))
 
+        self._formatFilepaths()
+
         # For back-compatibility...
         self.params = self
 
         # Run some basic validation
+        # ADW: This should be run after creating filenames
         self._validate()
 
-        # Possible filenames from this config (masked by existence)
-        try:
-            self.filenames = self.getFilenames()
-            self._makeFilenames()
-        except:
-            exc_type,exc_value,exc_traceback = sys.exc_info()
-            logger.warning("%s %s"%(exc_type,exc_value))
-            logger.warning("Filenames could not be created for config.")
+        # Filenames from this config (masked by existence) 
+        # ADW: We should not recreate filenames if they already exist
+        # in the input config
+        if not hasattr(self,'filenames'):
+            try:
+                self.filenames = self._createFilenames()
+            except:
+                exc_type,exc_value,exc_traceback = sys.exc_info()
+                logger.warning("%s %s"%(exc_type,exc_value))
+                logger.warning("Filenames could not be created for config.")
 
     def __str__(self):
         return yaml.dump(self)
@@ -117,8 +123,14 @@ class Config(dict):
             if not np.all(found):
                 msg = 'Missing keys in %s: '%(section)+str(keys[~found])
                 raise Exception(msg)
+
+        #if hasattr(self,'filenames'):
+        #    len(self.filenames)
             
-    def _makeFilenames(self):
+    def _formatFilepaths(self):
+        """
+        Join dirnames and filenames from config.
+        """
         likedir=self['output']['likedir']
         self.likefile  = join(likedir,self['output']['likefile'])
         self.mergefile = join(likedir,self['output']['mergefile'])
@@ -133,9 +145,20 @@ class Config(dict):
         mcmcdir=self['output']['mcmcdir']
         self.mcmcfile   = join(mcmcdir,self['output']['mcmcfile'])
 
-    def write(self, outfile):
-        ext = os.path.splitext(outfile)[1]
-        writer = open(outfile, 'w')
+    def write(self, filename):
+        """
+        Write a copy of this config object.
+
+        Parameters:
+        -----------
+        outfile : output filename
+        
+        Returns:
+        --------
+        None
+        """
+        ext = os.path.splitext(filename)[1]
+        writer = open(filename, 'w')
         if ext == '.py':
             writer.write(pprint.pformat(self))
         elif ext == '.yaml':
@@ -145,13 +168,10 @@ class Config(dict):
             raise Exception('Unrecognized config format: %s'%ext)
         writer.close()
 
-    def getFilenames(self,pixels=None):
+    def _createFilenames(self,pixels=None):
         """
         Create a masked records array of all filenames for the given set of
         pixels and store the existence of those files in the mask values.
-        If pixels is None, default behavior is to try to join grab
-        dirname + basename with no pixel insertion. If pixels == -1, grab
-        all pixels of 'nside_catalog'.
 
         Examples:
         f = getFilenames([1,2,3])
@@ -165,12 +185,21 @@ class Config(dict):
         f['mask_1'][f.mask['mask_1']]
         # Pixels where all files exist
         f['pix'][~f.mask['pix']]
+
+        Parameters:
+        -----------
+        pixels : If pixels is None, grab all pixels of 'nside_catalog'.
+
+        Returns:
+        --------
+        recarray : pixels and mask value
         """
         nside_catalog = self['coords']['nside_catalog']
 
-        if nside_catalog is None:
-            pixels = [None]
-        elif pixels is not None:
+        # Deprecated: ADW 2018-06-17
+        #if nside_catalog is None:
+        #    pixels = [None]
+        if pixels is not None:
             pixels = [pixels] if np.isscalar(pixels) else pixels
         else:
             pixels = np.arange(hp.nside2npix(nside_catalog))   
@@ -185,11 +214,13 @@ class Config(dict):
         mask_base_2 = self['mask']['basename_2']
          
         data = np.ma.empty(npix,dtype=[('pix',int), ('catalog',object), 
-                                          ('mask_1',object), ('mask_2',object)])
+                                       ('mask_1',object), ('mask_2',object)])
         mask = np.ma.empty(npix,dtype=[('pix',bool), ('catalog',bool), 
-                                          ('mask_1',bool), ('mask_2',bool)])
+                                       ('mask_1',bool), ('mask_2',bool)])
         for ii,pix in enumerate(pixels):
             if pix is None:
+                # DEPRECTATED: ADW 2018-06-17
+                # This is not really being used anymore
                 catalog = os.path.join(catalog_dir,catalog_base)
                 mask_1 = os.path.join(mask_dir,mask_base_1)
                 mask_2 = os.path.join(mask_dir,mask_base_2)
@@ -209,7 +240,7 @@ class Config(dict):
         for name in ['catalog','mask_1','mask_2']:
             if np.all(mask[name]): logger.warn("All '%s' files masked"%name)
 
-        # 'pix' is masked if all files not present
+        # mask 'pix' if all files not present
         mask['pix'] = mask['catalog'] | mask['mask_1'] | mask['mask_2']
 
         if np.all(mask['pix']): logger.warn("All pixels masked")
@@ -218,6 +249,79 @@ class Config(dict):
         #return np.ma.mrecords.MaskedArray(data, mask, fill_value=[-1,'','',''])
         return np.ma.MaskedArray(data, mask, fill_value=[-1,'','',''])
 
+
+    def _createFilenames(self):
+        """
+        Create a masked records array of all filenames for the given set of
+        pixels and store the existence of those files in the mask values.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        recarray : pixels and mask value
+        """
+        nside_catalog = self['coords']['nside_catalog']
+        npix = hp.nside2npix(nside_catalog)
+        pixels = np.arange(npix)
+
+        catalog_dir = self['catalog']['dirname']
+        catalog_base = self['catalog']['basename']
+        catalog_path = os.path.join(catalog_dir,catalog_base)
+
+        mask_dir    = self['mask']['dirname']
+        mask_base_1 = self['mask']['basename_1']
+        mask_base_2 = self['mask']['basename_2']
+        mask_path_1 = os.path.join(mask_dir,mask_base_1)
+        mask_path_2 = os.path.join(mask_dir,mask_base_2)
+
+        data = np.ma.empty(npix,dtype=[('pix',int), ('catalog',object), 
+                                       ('mask_1',object), ('mask_2',object)])
+        mask = np.ma.empty(npix,dtype=[('pix',bool), ('catalog',bool), 
+                                       ('mask_1',bool), ('mask_2',bool)])
+
+        # Build the filenames
+        data['pix']     = pixels
+        data['catalog'] = np.char.mod(catalog_path,pixels)
+        data['mask_1']  = np.char.mod(mask_path_1,pixels)
+        data['mask_2']  = np.char.mod(mask_path_2,pixels)
+
+        # Build the mask of existing files using glob
+        mask['catalog'] = ~np.in1d(data['catalog'],glob.glob(catalog_dir+'/*'))
+        mask['mask_1']  = ~np.in1d(data['mask_1'],glob.glob(mask_dir+'/*'))
+        mask['mask_2']  = ~np.in1d(data['mask_2'],glob.glob(mask_dir+'/*'))
+
+        for name in ['catalog','mask_1','mask_2']:
+            if np.all(mask[name]): logger.warn("All '%s' files masked"%name)
+
+        # mask 'pix' if all files not present
+        mask['pix'] = mask['catalog'] | mask['mask_1'] | mask['mask_2']
+
+        if np.all(mask['pix']): logger.warn("All pixels masked")
+
+        return np.ma.MaskedArray(data, mask, fill_value=[-1,'','',''])
+
+    def getFilenames(self,pixels=None):
+        """
+        Return the requested filenames.
+
+        Parameters:
+        -----------
+        pixels : requeseted pixels
+
+        Returns:
+        --------
+        filenames : recarray
+        """
+        logger.debug("Getting filenames...")
+        if pixels is None:
+            return self.filenames
+        else:
+            return self.filenames[np.in1d(self.filenames['pix'],pixels)]
+
+        
     getCatalogFiles = getFilenames
 
 ############################################################
