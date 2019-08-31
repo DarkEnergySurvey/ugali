43750ba7efb4a40ec0fd1b9a7631f2e1b1a5b0d8
Alex Drlica-Wagner <kadrlica@fnal.gov>
2019-08-31 13:51:05 -0500

    Adding extension (again) and defining difficulty cuts

--- ugali/scratch/simulation/simulate_population.py
+++ ugali/scratch/simulation/simulate_population.py
@@ -115,27 +115,28 @@ def catsimSatellite(config, lon_centroid, lat_centroid, distance, stellar_mass, 
     s = ugali.analysis.source.Source()
 
     # Following McConnachie 2012, ellipticity = 1 - (b/a) , where a is semi-major axis and b is semi-minor axis
-    
-    r_h = np.degrees(np.arcsin(r_physical / distance)) # Azimuthally averaged half-light radius
-    #ellipticity = 0.3 # Semi-arbitrary default for testing purposes
     # See http://iopscience.iop.org/article/10.3847/1538-4357/833/2/167/pdf
     # Based loosely on https://arxiv.org/abs/0805.2945
-    ellipticity = np.random.uniform(0.1, 0.8)
+    ellipticity = np.random.uniform(0.1, 0.8)    # Ellipticity [e = 1 - (b/a)]
     position_angle = np.random.uniform(0., 180.) # Random position angle (deg)
-    a_h = r_h / np.sqrt(1. - ellipticity) # semi-major axis (deg)
-    
-        
-    # Elliptical kernels take the "extension" as the semi-major axis
-    ker = ugali.analysis.kernel.EllipticalPlummer(lon=lon_centroid, lat=lat_centroid, ellipticity=ellipticity, position_angle=position_angle)
+    r_h = np.degrees(np.arcsin(r_physical / distance)) # Azimuthally averaged projected half-light radius (deg)
+    a_h = r_h / np.sqrt(1. - ellipticity) # Elliptical half-light radius along semi-major axis (deg)
 
+    # Create the kernel without extension
+    ker = ugali.analysis.kernel.EllipticalPlummer(lon=lon_centroid, lat=lat_centroid, ellipticity=ellipticity, position_angle=position_angle)
+    # Apply a max extension cut
     flag_too_extended = False
-    if a_h >= 1.0:
+    max_extension = 5.0 # deg
+    if a_h >= max_extension:
         print 'Too extended: a_h = %.2f'%(a_h)
-        a_h = 1.0
+        a_h = max_extension
         flag_too_extended = True
-    ker.setp('extension', value=a_h, bounds=[0.0,1.0])
+    # Elliptical kernels take the "extension" as the semi-major axis
+    extension = a_h # Elliptical half-light radius
+    ker.setp('extension', value=a_h, bounds=[0.0,max_extension])
     s.set_kernel(ker)
-    
+
+    # Create the isochrone
     age = np.random.choice([10., 12.0, 13.5])
     metal_z = np.random.choice([0.0001, 0.0002])
     distance_modulus = ugali.utils.projector.distanceToDistanceModulus(distance)
@@ -194,7 +195,7 @@ def catsimSatellite(config, lon_centroid, lat_centroid, distance, stellar_mass, 
     #abs_mag_martin = s.isochrone.absolute_magnitude_martin(richness=richness, n_trials=10)[0] # 100 trials seems to be sufficient for rough estimate
     #print 'abs_mag_martin = %.2f mag'%(abs_mag_martin)
 
-    # The more clever thing to do would be to sum up the actual simulated stars
+    # The more clever thing is to sum the simulated stars
     if config['survey'] == 'des':
         v = mag_1 - 0.487*(mag_1 - mag_2) - 0.0249 # See https://github.com/DarkEnergySurvey/ugali/blob/master/ugali/isochrone/model.py
     elif config['survey'] == 'ps1':
@@ -205,15 +206,13 @@ def catsimSatellite(config, lon_centroid, lat_centroid, distance, stellar_mass, 
         v = mag_1 + C_0 + C_1 * (mag_1 - mag_2)
     flux = np.sum(10**(-v/2.5))
     abs_mag = -2.5*np.log10(flux) - distance_modulus
-
     #print abs_mag, abs_mag_martin
 
-    #distance = ugali.utils.projector.distanceModulusToDistance(distance_modulus)
-    #r_h = extension * np.sqrt(1. - ellipticity) # Azimuthally averaged half-light radius
-    r_physical = distance * np.tan(np.radians(r_h)) # Azimuthally averaged half-light radius, kpc
+    # Realized surface brightness within azimuthally averaged half-light radius
+    surface_brightness = ugali.analysis.results.surfaceBrightness(abs_mag, r_physical, distance) 
+
     #print 'distance = %.3f kpc'%(distance)
     #print 'r_physical = %.3f kpc'%(r_physical)
-    surface_brightness = ugali.analysis.results.surfaceBrightness(abs_mag, r_physical, distance) # Average within azimuthally averaged half-light radius
     #print 'surface_brightness = %.3f mag arcsec^-2'%(surface_brightness)
     
     if plot:
@@ -234,11 +233,7 @@ def catsimSatellite(config, lon_centroid, lat_centroid, distance, stellar_mass, 
         print 'n_Sigma_p = %i'%(n_sigma_p)
         raw_input('WAIT')
         
-    #if flag_too_extended:
-    #    # This is a kludge to remove these satellites. fragile!!
-    #    n_g24 = 1.e6
-
-    return lon[cut_detect], lat[cut_detect], mag_1_meas[cut_detect], mag_2_meas[cut_detect], mag_1_error[cut_detect], mag_2_error[cut_detect], mag_extinction_1[cut_detect], mag_extinction_2[cut_detect], n_g22, n_g24, abs_mag, surface_brightness, ellipticity, position_angle, age, metal_z, flag_too_extended
+    return lon[cut_detect], lat[cut_detect], mag_1_meas[cut_detect], mag_2_meas[cut_detect], mag_1_error[cut_detect], mag_2_error[cut_detect], mag_extinction_1[cut_detect], mag_extinction_2[cut_detect], n_g22, n_g24, abs_mag, surface_brightness, extension, ellipticity, position_angle, age, metal_z, flag_too_extended
 
 ############################################################
 
@@ -268,7 +263,8 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
     range_distance = config.get('range_distance',[5., 500.])
     range_stellar_mass = config.get('range_stellar_mass',[1.e1, 1.e6])
     range_r_physical = config.get('range_r_physical',[1.e-3, 2.0])
-    
+    known_dwarfs = config.get('known_dwarfs',None)
+
     m_density = np.load(infile_density)
     nside_density = healpy.npix2nside(len(m_density))
     m_fracdet = read_map(infile_fracdet, nest=False) #.astype(np.float16)
@@ -283,16 +279,25 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
 
     mask = (m_fracdet > 0.5)
 
-    kwargs = dict(range_distance = range_distance,
-                  range_stellar_mass = range_stellar_mass,
-                  range_r_physical = range_r_physical)
-    print kwargs
-    # r_physical is azimuthally-averaged half-light radius, kpc
-    simulation_area, lon_population, lat_population, distance_population, stellar_mass_population, r_physical_population = ugali.simulation.population.satellitePopulation(mask, nside_pix, n, **kwargs)
+    if known_dwarfs is not None:
+        # Simulate from known dwarfs
+        print known_darfs
+        population = ugali.simulation.population.knownPopulation(known_dwarfs, mask, nside_pix, n)
+    else:
+        # r_physical is azimuthally-averaged half-light radius, kpc
+        kwargs = dict(range_distance = range_distance,
+                      range_stellar_mass = range_stellar_mass,
+                      range_r_physical = range_r_physical)
+        print kwargs
+        population = ugali.simulation.population.satellitePopulation(mask, nside_pix, n, **kwargs)
+
+    simulation_area, lon_population, lat_population, distance_population, stellar_mass_population, r_physical_population = population
+
     n_g22_population = np.tile(np.nan, n)
     n_g24_population = np.tile(np.nan, n)
     abs_mag_population = np.tile(np.nan, n)
     surface_brightness_population = np.tile(np.nan, n)
+    extension_population = np.tile(np.nan, n)
     ellipticity_population = np.tile(np.nan, n)
     position_angle_population = np.tile(np.nan, n)
     age_population = np.tile(np.nan, n)
@@ -313,7 +318,7 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
     for ii, mc_source_id in enumerate(mc_source_id_population):
         print '  Simulating satellite (%i/%i) ... MC_SOURCE_ID = %i'%(ii + 1, n, mc_source_id)
         print '  distance=%.2e, stellar_mass=%.2e, rhalf=%.2e'%(distance_population[ii],stellar_mass_population[ii],r_physical_population[ii])
-        lon, lat, mag_1, mag_2, mag_1_error, mag_2_error, mag_extinction_1, mag_extinction_2, n_g22, n_g24, abs_mag, surface_brightness, ellipticity, position_angle, age, metal_z, flag_too_extended = catsimSatellite(config,
+        lon, lat, mag_1, mag_2, mag_1_error, mag_2_error, mag_extinction_1, mag_extinction_2, n_g22, n_g24, abs_mag, surface_brightness, extension, ellipticity, position_angle, age, metal_z, flag_too_extended = catsimSatellite(config,
                                                                                                                                                                              lon_population[ii], 
                                                                                                                                                                              lat_population[ii], 
                                                                                                                                                                              distance_population[ii], 
@@ -327,13 +332,12 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
         n_g22_population[ii] = n_g22
         n_g24_population[ii] = n_g24
         abs_mag_population[ii] = abs_mag
+        extension_population[ii] = extension
         surface_brightness_population[ii] = surface_brightness
         ellipticity_population[ii] = ellipticity
         position_angle_population[ii] = position_angle
         age_population[ii] = age
         metal_z_population[ii] = metal_z
-
-        #print "Difficulty masking..."  
 
         # These objects are too extended and are not simulated
         if (flag_too_extended):
@@ -341,13 +345,18 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
 
         # We assume that these objects would be easily detected and
         # remove them to reduce data volume
-        if (surface_brightness_population[ii]<25.)&(n_g22_population[ii]>1e2):
+        if (surface_brightness_population[ii]<23.5)&(n_g22_population[ii]>1e3):
             difficulty_population[ii] |= 0b0010
-        if (surface_brightness_population[ii]<28.)&(n_g22_population[ii]>1e4):
-            difficulty_population[ii] |= 0b0100
-        if (surface_brightness_population[ii]<30.)&(n_g22_population[ii]>1e5):
-            difficulty_population[ii] |= 0b1000
+
+        # ADW 2019-08-31: I don't think these were implmented
+        #if (surface_brightness_population[ii]<25.)&(n_g22_population[ii]>1e2):
+        #    difficulty_population[ii] |= 0b0010
+        #if (surface_brightness_population[ii]<28.)&(n_g22_population[ii]>1e4):
+        #    difficulty_population[ii] |= 0b0100
+        #if (surface_brightness_population[ii]<30.)&(n_g22_population[ii]>1e5):
+        #    difficulty_population[ii] |= 0b1000
         
+        # ADW: 2019-08-31: These were Keith's original cuts, which were too aggressive
         #cut_easy = (surface_brightness_population[ii]<25.)&(n_g22_population[ii]>1.e2) \
         #           | ((surface_brightness_population[ii] < 30.) & (n_g24_population[ii] > 1.e4)) \
         #           | ((surface_brightness_population[ii] < 31.) & (n_g24_population[ii] > 1.e5))
@@ -360,6 +369,7 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
         #if flag_too_extended:
         #    difficulty_population[ii] += 3 # TOO EXTENDED
 
+        # Only write satellites that aren't flagged
         if difficulty_population[ii] == 0:
             lon_array.append(lon)
             lat_array.append(lat)
@@ -371,8 +381,7 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
             mag_extinction_2_array.append(mag_extinction_2)
             mc_source_id_array.append(np.tile(mc_source_id, len(lon)))
 
-    # Concatenate all the arrays
-
+    # Concatenate the arrays
     print "Concatenating arrays..."
     lon_array = np.concatenate(lon_array)
     lat_array = np.concatenate(lat_array)
@@ -385,7 +394,6 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
     mc_source_id_array = np.concatenate(mc_source_id_array)
 
     # Now do the masking all at once
-
     print "Fracdet masking..."
     pix_array = ugali.utils.healpix.angToPix(nside_fracdet, lon_array, lat_array)
     cut_fracdet = (np.random.uniform(size=len(lon_array)) < m_fracdet[pix_array])
@@ -400,25 +408,7 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
     mag_extinction_2_array = mag_extinction_2_array[cut_fracdet]
     mc_source_id_array = mc_source_id_array[cut_fracdet]
 
-    # Cut out the entries that are easily detectable
-    """
-    lon_population = lon_population[cut_difficulty_population]
-    lat_population = lat_population[cut_difficulty_population]
-    distance_population = distance_population[cut_difficulty_population]
-    stellar_mass_population = stellar_mass_population[cut_difficulty_population]
-    r_physical_population = r_physical_population[cut_difficulty_population]
-    n_g24_population = n_g24_population[cut_difficulty_population]
-    abs_mag_population = abs_mag_population[cut_difficulty_population]
-    surface_brightness_population = surface_brightness_population[cut_difficulty_population]
-    ellipticity_population = ellipticity_population[cut_difficulty_population]
-    position_angle_population = position_angle_population[cut_difficulty_population]
-    age_population = age_population[cut_difficulty_population]
-    metal_z_population = metal_z_population[cut_difficulty_population]
-    mc_source_id_population = mc_source_id_population[cut_difficulty_population]
-    """
-    
     # Create bonus columns
-    
     print "Creating bonus columns..."
     distance_modulus_population = ugali.utils.projector.distanceToDistanceModulus(distance_population)
     hpix_32_population = ugali.utils.healpix.angToPix(32, lon_population, lat_population) # Make sure this matches the dataset
@@ -458,56 +448,15 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
         coadd_object_id_array.append((1000000 * mc_source_id) + 1 + np.arange(np.sum(mc_source_id == mc_source_id_array)))
     coadd_object_id_array = -1 * np.concatenate(coadd_object_id_array) # Assign negative numbers to distinguish from real objects
 
-    # Catalog output file
+    # Simulated catalog output needs to match the real data
+    #   https://github.com/sidneymau/simple/blob/master/search_algorithm.py 
+    #   https://github.com/sidneymau/simple/blob/master/config.yaml
+    #   /home/s1/kadrlica/projects/y3a2/dsphs/v2/skim/ 
+    #   e.g., /home/s1/kadrlica/projects/y3a2/dsphs/v2/skim/y3a2_ngmix_cm_11755.fits
 
     # for ii in range(0, len(d.formats)): print '\'%s\': [ , \'%s\'],'%(d.names[ii], d.formats[ii])
-    
-    # See: 
-    # https://github.com/sidneymau/simple/blob/master/search_algorithm.py 
-    # https://github.com/sidneymau/simple/blob/master/config.yaml
-    # /home/s1/kadrlica/projects/y3a2/dsphs/v2/skim/ , e.g., /home/s1/kadrlica/projects/y3a2/dsphs/v2/skim/y3a2_ngmix_cm_11755.fits
 
-    #default_array = np.tile(np.nan, len(mc_source_id_array)) # To recognize that those values are synthetic filler
     default_array = np.tile(-9999., len(mc_source_id_array))
-
-    """
-    # Column name, data, fits format
-    # Y3A2 pre-Gold
-    key_map = {'CM_MAG_ERR_G': [mag_1_error_array, 'D'],
-               'CM_MAG_ERR_R': [mag_2_error_array, 'D'],
-               'CM_MAG_G': [mag_1_array, 'D'],
-               'CM_MAG_R': [mag_2_array, 'D'],
-               'CM_T': [default_array, 'D'],
-               'CM_T_ERR': [default_array, 'D'],
-               'COADD_OBJECT_ID': [coadd_object_id_array, 'K'],
-               'DEC': [lat_array, 'D'],
-               'FLAGS': [default_array, 'K'],
-               'PSF_MAG_ERR_G': [mag_1_error_array, 'D'],
-               'PSF_MAG_ERR_R': [mag_2_error_array, 'D'],
-               'PSF_MAG_G': [mag_1_array, 'D'],
-               'PSF_MAG_R': [mag_2_array, 'D'],
-               'RA': [lon_array, 'D'],
-               'SEXTRACTOR_FLAGS_G': [np.tile(0, len(mc_source_id_array)), 'I'],
-               'SEXTRACTOR_FLAGS_R': [np.tile(0, len(mc_source_id_array)), 'I'],
-               'WAVG_MAG_PSF_G': [mag_1_array, 'E'],
-               'WAVG_MAG_PSF_R': [mag_2_array, 'E'],
-               'WAVG_MAGERR_PSF_G': [mag_1_error_array, 'E'],
-               'WAVG_MAGERR_PSF_R': [mag_2_error_array, 'E'],
-               'WAVG_SPREAD_MODEL_I': [default_array, 'E'],
-               'WAVG_SPREADERR_MODEL_I': [default_array, 'E'],
-               'EXT_SFD98_G': [default_array, 'E'],
-               'EXT_SFD98_R': [default_array, 'E'],
-               'CM_MAG_SFD_G': [mag_1_array, 'D'],
-               'CM_MAG_SFD_R': [mag_2_array, 'D'],
-               'FLAG_FOOTPRINT': [np.tile(1, len(mc_source_id_array)), 'J'],
-               'FLAG_FOREGROUND': [np.tile(0, len(mc_source_id_array)), 'J'],
-               'EXTENDED_CLASS_MASH': [np.tile(0, len(mc_source_id_array)), 'K'],
-               'PSF_MAG_SFD_G': [mag_1_array, 'D'],
-               'PSF_MAG_SFD_R': [mag_2_array, 'D'],
-               'WAVG_MAG_PSF_SFD_G': [mag_1_array, 'E'],
-               'WAVG_MAG_PSF_SFD_R': [mag_2_array, 'E']}
-    """
-    
     if config['survey'] == 'des':
         # Y3 Gold v2.0
         key_map = odict([
@@ -595,11 +544,10 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
         header = copy.deepcopy(tbhdu.header)
         header.set('IDMIN',mc_source_id_chunk[0], 'Minimum MC_SOURCE_ID')
         header.set('IDMAX',mc_source_id_chunk[-1], 'Maximum MC_SOURCE_ID')
-        pyfits.writeto(outfile, tbhdu.data[cut_chunk], header, clobber=True)
+        pyfits.writeto(outfile, tbhdu.data[cut_chunk], header, overwrite=True)
 
     # Population metadata output file
     
-    print "Writing population metadata file..."
     tbhdu = pyfits.BinTableHDU.from_columns([
         pyfits.Column(name='RA', format='E', array=lon_population, unit='deg'),
         pyfits.Column(name='DEC', format='E', array=lat_population, unit='deg'),
@@ -613,6 +561,7 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
         pyfits.Column(name='DIFFICULTY', format='J', array=difficulty_population, unit=''),
         pyfits.Column(name='ABS_MAG', format='E', array=abs_mag_population, unit='mag'),
         pyfits.Column(name='SURFACE_BRIGHTNESS', format='E', array=surface_brightness_population, unit='mag arcsec^-2'),
+        pyfits.Column(name='EXTENSION', format='E', array=extension_population, unit='deg'),
         pyfits.Column(name='ELLIPTICITY', format='E', array=ellipticity_population, unit=''),
         pyfits.Column(name='POSITION_ANGLE', format='E', array=position_angle_population, unit='deg'),
         pyfits.Column(name='AGE', format='E', array=age_population, unit='deg'),
@@ -629,12 +578,11 @@ def catsimPopulation(tag, mc_source_id_start=1, n=5000, n_chunk=100, config='sim
         pyfits.Column(name='SURVEY', format='A12', array=survey_population, unit=''),
     ])
     tbhdu.header.set('AREA', simulation_area, 'Simulation area (deg^2)')
-    tbhdu.writeto('%s/sim_population_%s_mc_source_id_%07i-%07i.fits'%(tag, tag, mc_source_id_start, mc_source_id_start + n - 1), clobber=True)
-
-    # 5284.2452461023322
+    print "Writing population metadata file..."
+    filename = '%s/sim_population_%s_mc_source_id_%07i-%07i.fits'%(tag, tag, mc_source_id_start, mc_source_id_start + n - 1)
+    tbhdu.writeto(filename, overwrite=True)
 
     # Mask output file
-
     print "Writing population mask file..."
     outfile_mask = '%s/sim_mask_%s_cel_nside_%i.fits'%(tag, tag, healpy.npix2nside(len(mask)))
     if not os.path.exists(outfile_mask):
@@ -662,7 +610,6 @@ if __name__ == "__main__":
                         help="Random seed")
     args = parser.parse_args()
 
-    #print args
     if args.seed is not None: 
         print("Setting random seed: %i"%args.seed)
         np.random.seed(args.seed)
