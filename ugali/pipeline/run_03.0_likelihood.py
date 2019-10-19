2ee0504d744075e14a33ca76bfb58207323adb67
Alex Drlica-Wagner <kadrlica@fnal.gov>
2019-10-19 15:53:20 -0700

    chunk merging; add check functionality

--- ugali/pipeline/run_03.0_likelihood.py
+++ ugali/pipeline/run_03.0_likelihood.py
@@ -2,7 +2,9 @@
 """Run the likelihood search."""
 import glob
 import os
-from os.path import join, exists
+from os.path import join, exists, basename
+
+import numpy as np
 
 from ugali.analysis.farm import Farm
 from ugali.analysis.pipeline import Pipeline
@@ -10,9 +12,9 @@ from ugali.analysis.pipeline import Pipeline
 from ugali.utils.shell import mkdir
 from ugali.utils.logger import logger
 import ugali.utils.skymap
-import ugali.utils.healpix
+from ugali.utils import healpix
 
-components = ['scan','merge','tar','plot']
+components = ['scan','merge','tar','plot','check']
 defaults = ['scan','merge']
 
 def run(self):
@@ -26,21 +28,30 @@ def run(self):
         mergefile = self.config.mergefile
         roifile = self.config.roifile
         filenames = self.config.likefile.split('_%')[0]+'_*.fits'
-        infiles = sorted(glob.glob(filenames))
+        infiles = np.array(sorted(glob.glob(filenames)))
 
-        if exists(mergefile) and not self.opts.force:
-            logger.warn("  Found %s; skipping..."%mergefile)
-        else:
-            logger.info("  Merging likelihood files...")
-            ugali.utils.healpix.merge_partial_maps(infiles,mergefile)
+        if 'mergedir' in self.config['output']:
+            mkdir(self.config['output']['mergedir'])
+
+        pixels = np.char.rpartition(np.char.rpartition(infiles,'_')[:,0],'_')[:,-1]
+        pixels = pixels.astype(int)
+        superpixel = healpix.superpixel(pixels,
+                                        self.config['coords']['nside_likelihood'],
+                                        self.config['coords']['nside_merge'])
+                         
+        for pix in np.unique(superpixel):
+            outfile = mergefile%pix
+            if exists(outfile) and not self.opts.force:
+                logger.warn("  Found %s; skipping..."%outfile)
+            else:
+                healpix.merge_partial_maps(infiles[superpixel == pix],
+                                           outfile,multiproc=8)
 
         if exists(roifile) and not self.opts.force:
             logger.warn("  Found %s; skipping..."%roifile)
         else:
             logger.info("  Merging likelihood headers...")
-            ugali.utils.healpix.merge_likelihood_headers(infiles,roifile)
-
-            #ugali.utils.skymap.mergeLikelihoodFiles(infiles,mergefile,roifile)
+            healpix.merge_likelihood_headers(infiles,roifile)
             
     if 'tar' in self.opts.run:
         logger.info("Running 'tar'...")
@@ -74,6 +85,34 @@ def run(self):
         outfile = os.path.join(outdir,basename)
         plt.savefig(outfile)
 
+    if 'check' in self.opts.run:
+        # Check the completion fraction
+        logger.info("Running 'check'...")
+
+        import fitsio
+        import numpy as np
+        import healpy as hp
+
+        from ugali.utils.skymap import inFootprint
+
+        # Load the ROI file
+        roi = fitsio.read(self.config.roifile)
+        done = roi['PIXEL']
+        
+        # Get all target pixels
+        nside = self.config['coords']['nside_likelihood']
+        pixels = np.arange(hp.nside2npix(nside))
+        pixarea = hp.nside2pixarea(nside,degrees=True)
+        foot = pixels[inFootprint(self.config,pixels)]
+
+        # And find the pixels that haven't been processed
+        undone = ~np.in1d(foot,done)
+        hpxmap = np.zeros(len(pixels))
+        hpxmap[foot[undone]] = True
+
+        logger.info("Found %i incomplete pixels with an area of %.1f deg^2."%(hpxmap.sum(), hpxmap.sum()*pixarea))
+        hp.write_map('check.fits.gz',hpxmap)
+        
 Pipeline.run = run
 Pipeline.defaults = defaults
 pipeline = Pipeline(__doc__,components)
